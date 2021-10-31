0036a9fb2bbf6f3319ace0a7ded7524574cca26f
Dragos Teodor Maican <58427062+Kcricket@users.noreply.github.com>
2021-10-31 12:03:16 +0100

    First Addings

---
+++ Tema1/Worksheet1/Ej1.js
@@ -0,0 +1,7 @@
+var a = parseInt(prompt("Introduzca la primera variable"));
+var b = parseInt(prompt("Introduzca la segunda variable"));
+
+document.write("La suma es: "+(a+b)+"</p>");
+document.write("La resta es: "+(a-b)+"</p>");
+document.write("El producto es: "+(a*b)+"</p>");
+document.write("La division es: "+(a/b)+"</p>");
